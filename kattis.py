270959215ea648e83f93256bc1ec354c76af4023
jebt <roelantvanderhilst@gmail.com>
2023-03-12 09:08:40 +0100

    refactored the code for formatting the template to be much more concise

--- kattis.py
+++ kattis.py
@@ -74,10 +74,11 @@ def set_up_next():
         # change problem_id = "..." (if possible during runtime)
         change_code(next_problem_id)
         # import problems.{problem_id} as problem and run it
-        set_and_run_problem_module(next_problem_path)
+        run_problem_module(next_problem_path)
 
 
-def set_and_run_problem_module(problem_path):
+def run_problem_module(problem_path):
+    print(problem_path)
     raise NotImplementedError
     # probably first set the module level problem_id variable and then call a separate function to run it (which should
     # also be used in the main function normally)
@@ -108,89 +109,80 @@ def scrape_and_create_problem_module(next_problem_id: str, next_problem_path: st
     url = f"https://open.kattis.com/problems/{next_problem_id}"
     response = requests.get(url)
     if response.status_code == 200:
-        text = str(response.content)
+        problem_text = str(response.content)
     else:
         print('Request failed with status code:', response.status_code)
         raise NotImplementedError
-    with open("template.py") as f:
-        new_module = f.read()
-    if "<th>Sample Output 1</th>" in text:
-        samples_from_i = text.index("<th>Sample Output 1</th>")
-        text = text[samples_from_i::]
-        pattern = re.compile(r'<pre>(.*?)</pre>', re.DOTALL)
-        matches = pattern.findall(text)
-        match_count = len(matches)
-        i1, i2, i3, i4, o1, o2, o3, o4 = [None] * 8
-        if match_count == 0:
-            raise ValueError("No samples?")
-        if match_count % 2 != 0:
-            raise ValueError("Uneven number of sample inputs + sample outputs?")
-        if match_count > 8:
-            raise ValueError("More than 4 sample input+output pairs?")
-        if match_count >= 2:
-            i1 = matches[0]
-            o1 = matches[1]
-        if match_count >= 4:
-            i2 = matches[2]
-            o2 = matches[3]
-        if match_count >= 6:
-            i3 = matches[4]
-            o3 = matches[5]
-        if match_count >= 8:
-            i4 = matches[6]
-            o4 = matches[7]
 
-        if match_count < 8:
-            new_module = new_module.replace('''
-    (
-        # Input 4
-        """input_4""",
-        # Output 4
-        """output_4"""
-    ),''', "")
-        if match_count < 6:
-            new_module = new_module.replace('''
-    (
-        # Input 3
-        """input_3""",
-        # Output 3
-        """output_3"""
-    ),''', "")
-        if match_count < 4:
-            new_module = new_module.replace('''
-    (
-        # Input 2
-        """input_2""",
-        # Output 2
-        """output_2"""
-    ),''', "")
-            if match_count < 2:
-                new_module = new_module.replace('''
-    (
-        # Input 1
-        """input_1""",
-        # Output 1
-        """output_1"""
-    ),''', "")
-
-        if match_count > 0:
-            new_module = new_module.replace("input_1", i1)
-            new_module = new_module.replace("output_1", o1)
-        if match_count > 2:
-            new_module = new_module.replace("input_2", i2)
-            new_module = new_module.replace("output_2", o2)
-        if match_count > 4:
-            new_module = new_module.replace("input_3", i3)
-            new_module = new_module.replace("output_3", o3)
-        if match_count > 6:
-            new_module = new_module.replace("input_4", i4)
-            new_module = new_module.replace("output_4", o4)
+    if "<th>Sample Output 1</th>" in problem_text:
+        new_module = create_prepared_module(problem_text)
     else:
         print("No samples found!")
         raise NotImplementedError
 
     with open(next_problem_path, "w") as file:
         file.write(new_module)
+
+
+def create_prepared_module(problem_text):
+    samples_from_i = problem_text.index("<th>Sample Output 1</th>")
+    problem_text = problem_text[samples_from_i::]
+    pattern = re.compile(r'<pre>(.*?)</pre>', re.DOTALL)
+    matches = pattern.findall(problem_text)
+    match_count = len(matches)
+    if match_count == 0:
+        raise ValueError("No samples?")
+    if match_count % 2 != 0:
+        raise ValueError("Uneven number of sample inputs + sample outputs?")
+    if match_count > 8:
+        raise ValueError("More than 4 sample input+output pairs?")
+    io_pairs = match_count // 2
+    new_module = replace_template_io(io_pairs, matches)
+
+    # delete unused input/output samples from the template
+    for i in range(io_pairs + 1, 5):
+        new_module = new_module.replace(f'''
+    (
+        # Input {i}
+        """input_{i}""",
+        # Output {i}
+        """output_{i}"""
+    ),''', "")
+    return new_module
+
+
+def replace_template_io(io_pairs, matches):
+    with open("template.py") as f:
+        new_module = f.read()
+    for i in range(io_pairs):
+        new_module = new_module.replace(f"input_{i + 1}", matches[i])
+        new_module = new_module.replace(f"output_{i + 1}", matches[i + 1])
+    # i1, i2, i3, i4, o1, o2, o3, o4 = [None] * 8
+    # if io_pairs >= 1:
+    #     i1 = matches[0]
+    #     o1 = matches[1]
+    # if io_pairs >= 2:
+    #     i2 = matches[2]
+    #     o2 = matches[3]
+    # if io_pairs >= 3:
+    #     i3 = matches[4]
+    #     o3 = matches[5]
+    # if io_pairs >= 4:
+    #     i4 = matches[6]
+    #     o4 = matches[7]
+    # if io_pairs > 0:
+    #     new_module = new_module.replace("input_1", i1)
+    #     new_module = new_module.replace("output_1", o1)
+    # if io_pairs > 1:
+    #     new_module = new_module.replace("input_2", i2)
+    #     new_module = new_module.replace("output_2", o2)
+    # if io_pairs > 2:
+    #     new_module = new_module.replace("input_3", i3)
+    #     new_module = new_module.replace("output_3", o3)
+    # if io_pairs > 3:
+    #     new_module = new_module.replace("input_4", i4)
+    #     new_module = new_module.replace("output_4", o4)
+    return new_module
 
 
 def find_next_problem_id():
@@ -210,15 +202,9 @@ def find_next_problem_id():
 
 def extract_next_problem_id(response_content):
     text = str(response_content)
-
-    # pattern = re.compile(
-    #     r'<td><a href="/problems/(.*?)">(.*?)</a></td>\n\s+<td>\n\s+<div class="status is-status-untried">' +
-    #     r'<i class="fas fa-times status-icon is-not-attempted"></i>Not Attempted</div>')
     pattern = re.compile(r'<td><a href="/problems/(.{1,64})">.{1,64}</a></td>\\n\s+\\n\s+\\n\s+<td>\\n\s+'
                          r'<div class="status is-status-untried">')
-
     match = pattern.search(text)
-
     if match:
         return match.group(1)
     else:
